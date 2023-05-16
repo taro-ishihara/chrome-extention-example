b0b9dff62d1f48ea1336574642583f5c0de49361
Taro Ishihara <taro.ishihara.mail@gmail.com>
2023-05-15 20:06:33 -0700

    anchor

--- content-script.js
+++ content-script.js
@@ -1,15 +1,5 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  switch (request) {
-    case "getSelectedText":
-      const selectedText = window.getSelection().toString();
-      sendResponse(selectedText);
-      break;
-  }
-});
-
-const regex = /extension/gi;
-
-function* getTargetNodes(query) {
+const getTargetNodes = function* (key) {
+  regex = new RegExp(key, "gi");
   const acceptNode = (node) => {
     if (/script|style/i.test(node.parentNode.nodeName)) {
       return NodeFilter.FILTER_REJECT;
@@ -17,7 +7,6 @@ function* getTargetNodes(query) {
     if (regex.test(node.nodeValue)) {
       return NodeFilter.FILTER_ACCEPT;
     }
-    //return NodeFilter.FILTER_ACCEPT;
   };
   const walker = document.createTreeWalker(
     document.body,
@@ -26,19 +15,49 @@ function* getTargetNodes(query) {
   );
 
   while (walker.nextNode()) yield walker.currentNode;
-}
+};
 
-const highlightWord = (node) => {
-  const highlightStyle = "background-color: green;";
+const highlightWord = (node, key, description) => {
+  regex = new RegExp(key, "gi");
   const newNode = document.createElement("span");
   newNode.innerHTML = node.nodeValue.replace(
     regex,
-    `<span style="${highlightStyle}">$&</span>`
+    `<span class="aidext-highlight"><abbr title="${description}">$&</abbr></span>`
   );
   node.parentNode.replaceChild(newNode, node);
 };
 
+const updatePage = async () => {
+  const items = await chrome.storage.sync.get(null);
+  for (const key in items) {
+    const targetNodes = [...getTargetNodes(key)];
+    for (const node of targetNodes) {
+      highlightWord(node, key, items[key]);
+    }
+  }
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  switch (request.message) {
+    case "getSelectedText":
+      const selectedText = window.getSelection().toString();
+      sendResponse(selectedText);
+      break;
+    case "updatePage":
+      updatePage();
+      break;
+  }
+});
+
 window.addEventListener("load", () => {
-  const targetNodes = [...getTargetNodes("extension")];
-  targetNodes.forEach(highlightWord);
+  updatePage();
+});
+
+document.addEventListener("selectionchange", function () {
+  const selection = window.getSelection();
+  if (selection.type !== "Range") {
+    return;
+  }
+  const selectedNode = selection.anchorNode;
+  console.log(selectedNode.nodeValue);
 });
--- popup.js
+++ popup.js
@@ -1,13 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const openApiKey = document.getElementById("open_api_key");
+  const value = (await chrome.storage.sync.get("openApiKey"))["openApiKey"];
+  openApiKey.value = value;
   openApiKey.addEventListener("input", () => {
-    chrome.storage.sync.set({ 'openApiKey': openApiKey.value });
+    chrome.storage.sync.set({ openApiKey: openApiKey.value });
   });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const promptTemplate = document.getElementById("prompt_template");
+  const value = (await chrome.storage.sync.get("promptTemplate"))[
+    "promptTemplate"
+  ];
+  promptTemplate.value = value;
   promptTemplate.addEventListener("input", () => {
-    chrome.storage.sync.set({ 'promptTemplate': promptTemplate.value });
+    chrome.storage.sync.set({ promptTemplate: promptTemplate.value });
   });
 });
